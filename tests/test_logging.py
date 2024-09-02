a6c93fa120722dfa90dc283b18c9cd9c4976fb55
Najeeb Ibrahim <79699737+najeebib@users.noreply.github.com>
2024-09-02 13:16:03 +0300

    Update test_logging.py

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,7 +2,7 @@ import os
 import pytest
 import logging
 import logging.config
-from bot.config.logging_config import logging_config
+from bot.config.logging_config import app_logger
 
 @pytest.fixture
 def log_file():
@@ -15,13 +15,9 @@ def log_file():
         os.remove(log_file_path)
 
 def test_logging(log_file):
-    # Configure logging for the test
-    logging.config.dictConfig(logging_config)
-    logger = logging.getLogger(__name__)
-
     # Log some test messages
-    logger.info("This is an info message for testing.")
-    logger.error("This is an error message for testing.")
+    app_logger.info("This is an info message for testing.")
+    app_logger.error("This is an error message for testing.")
 
     # Verify that the log messages were written to the log file
     with open(log_file, 'r') as f:
